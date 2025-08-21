33e8c92414bde228276b3e4269fae8340eebcce8
paulo3433 <paulo.tavares.santos@escola.pr.gov.br>
2025-08-21 15:11:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Educação Física',
+    'O que é resistência muscular?',
+    'Resistência muscular é a capacidade do músculo de realizar contrações repetidas sem se fatigar rapidamente.'
+)
+
+criaCartao(
+    'Educação Física',
+    'Qual a diferença entre força e potência muscular?',
+    'A força muscular é a capacidade de gerar força, enquanto a potência muscular envolve a capacidade de gerar força rapidamente.'
+)
+
+criaCartao(
+    'Educação Física',
+    'O que é a frequência cardíaca?',
+    'A frequência cardíaca é o número de batimentos do coração por minuto.'
+)
+
+criaCartao(
+    'Educação Física',
+    'Quais são os benefícios do alongamento?',
+    'O alongamento melhora a flexibilidade, reduz o risco de lesões e ajuda na recuperação muscular.'
+)
+
+criaCartao(
+    'Educação Física',
+    'O que é o índice de massa corporal (IMC)?',
+    'O IMC é uma medida que relaciona o peso e a altura de uma pessoa, utilizado para indicar se ela está dentro de uma faixa saudável.'
+)
+
+criaCartao(
+    'Educação Física',
+    'Qual é a principal diferença entre exercícios aeróbicos e anaeróbicos?',
+    'Exercícios aeróbicos são de longa duração e intensidade moderada, enquanto os anaeróbicos são de curta duração e alta intensidade.'
+)
+
+criaCartao(
+    'Educação Física',
+    'O que é a flexibilidade?',
+    'Flexibilidade é a capacidade de mover as articulações através de sua amplitude máxima de movimento.'
+)
+
+criaCartao(
+    'Educação Física',
+    'Quais são os principais tipos de músculos no corpo humano?',
+    'Os principais tipos de músculos são os esqueléticos, lisos e cardíacos.'
+)
+
+criaCartao(
+    'Educação Física',
+    'O que significa a sigla HIIT?',
+    'HIIT significa "High-Intensity Interval Training", que é um treinamento intervalado de alta intensidade.'
+)
+
+criaCartao(
+    'Educação Física',
+    'O que é a capacidade cardiorrespiratória?',
+    'Capacidade cardiorrespiratória é a habilidade do coração e dos pulmões de fornecer oxigênio aos músculos durante atividades prolongadas.'
+)
